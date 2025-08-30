40d339c51ecd9974cbfafb2d9438508937fb2a16
virajsaranga <virajridi@gmail.com>
2025-08-30 14:57:07 +0530

    update

--- frontend/src/pages/AddFood.js
+++ frontend/src/pages/AddFood.js
@@ -43,6 +43,7 @@ const AddFood = () => {
         }
     );
 
+    //  console.log(newFood);
     const handleChange = ({target}) => {
         setNewFood({...newFood, [target.name]: target.value});
     }
